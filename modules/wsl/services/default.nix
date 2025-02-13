d1b735c9acbc57443ce02b6bc18db4250dbfacc9
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-02-13 12:17:59 -0500

    feat: Enable networking in WSL that works when VPN is active

--- modules/wsl/services/default.nix
+++ modules/wsl/services/default.nix
@@ -1,11 +1,6 @@
-_: {
-  # systemd.services."enable-wsl-vpnkit" = {
-  #   description = "Enable wsl vpnkit";
-  #   wantedBy = ["multi-user.target"];
-  #   serviceConfig = {
-  #     ExecStart = "/mnt/c/Windows/system32/wsl.exe -d wsl-vpnkit --cd /app wsl-vpnkit";
-  #     Restart = "always";
-  #     KillMode = "mixed";
-  #   };
-  # };
+{ pkgs, ... }:
+{
+  environment.systemPackages = [
+    pkgs.wsl-vpnkit
+  ];
 }
