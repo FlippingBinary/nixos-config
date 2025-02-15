daf7d4988e8e0c2d82f1965a66775b9e632cfe72
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2025-02-14 22:13:09 -0500

    feat: Add VivoBook

---
+++ hosts/vivobook/default.nix
@@ -0,0 +1,108 @@
+{ ... }:
+{
+  imports = [ ./hardware.nix ];
+
+  networking = {
+    hostName = "vivobook";
+    hostId = "";
+    nameservers = [
+      "10.1.1.1"
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+
+    # Enable networking
+    networkmanager.enable = true;
+  };
+
+  musselwhite-dev = {
+    stateVersion = "24.05";
+    core = {
+      zfs = {
+        enable = true;
+        encrypted = true;
+        rootDataset = "rpool/local/root";
+      };
+    };
+    graphical = {
+      enable = true;
+      laptop = false;
+      hyprland = {
+        enable = true;
+      };
+      xdg = {
+        enable = true;
+      };
+    };
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # # Enable automatic login for the user.
+  # services.xserver.displayManager.autoLogin.enable = true;
+  # services.xserver.displayManager.autoLogin.user = "jon";
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "24.11"; # Did you read the comment?
+}
